18351fe9ac959f3774ae7b4e1cbe739d188eb741
Marvin <marvinraj77@gmail.com>
2024-04-07 00:14:08 +0800

    instructions and title

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 def main():
-    pass
+    instructions()
+
+    if play():
+        print("works")
+    else: 
+        print("it does not work bro")
+
+def play():
+    user_choice = input("Are you ready to play? (y)es or (n)o\n")
+    if user_choice == "n":
+        return False
+    elif user_choice == "y":
+        return True
+
+def get_words():
+    random_words = ["apple", "terminator", "basketball"]
+
+
+
+# function to display instructions
+def instructions():
+    print("\nH A N G  M A N")
+    print("\nWelcome to Hangman! A word will be chosen at random and \nyou must try to guess the word correctly letter by letter \nbefore you run out of attempts. Good luck!\n")
 
 if __name__ == "__main__":
     main()
