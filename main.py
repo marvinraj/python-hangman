3a7ef46402ba26c71dc39fb2552e2c58cd5f75a8
Marvin <marvinraj77@gmail.com>
2024-04-07 23:22:02 +0800

    dealt with storing and selecting random words

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
+import random
+
 def main():
     instructions()
+    play = True
+    secret_word = get_words()
+    chances = 10
+    guessed_letters = []
+    word_to_guess = []
 
-    if play():
-        print("works")
-    else: 
-        print("it does not work bro")
+    while play:
+        print('\nGuess the following word! Please select a letter between A-Z.\n')
 
-def play():
-    user_choice = input("Are you ready to play? (y)es or (n)o\n")
-    if user_choice == "n":
-        return False
-    elif user_choice == "y":
-        return True
+        # create blank version of the secret word
+        for letter in secret_word:
+            word_to_guess.append('_')
+        
+        # print out the blank version of the secret word
+        for i in word_to_guess:
+            print(i, end=" ")
+        
+        # print an empty line so that the code looks better
+        print("\n")
 
+        play = False
+
+# a function to store a list of random words and returns a random word from the list
 def get_words():
-    random_words = ["apple", "terminator", "basketball"]
+    random_words = ["apple", "orange", "lime"]
+    chosen_word = random.choice(random_words)
+    return chosen_word
 
 
-
-# function to display instructions
+# a function to display instructions
 def instructions():
     print("\nH A N G  M A N")
     print("\nWelcome to Hangman! A word will be chosen at random and \nyou must try to guess the word correctly letter by letter \nbefore you run out of attempts. Good luck!\n")
+
+# def play():
+#     user_choice = input("Are you ready to play? (y)es or (n)o\n")
+#     if user_choice == "n":
+#         return False
+#     elif user_choice == "y":
+#         return True
 
 if __name__ == "__main__":
     main()
