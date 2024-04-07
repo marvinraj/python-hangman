6ff71077a59a861ddd7d2991028ab796b072bd7b
Marvin <marvinraj77@gmail.com>
2024-04-08 00:08:31 +0800

    added the basic logic of the game

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
 import random
 
 def main():
+
     instructions()
-    play = True
-    secret_word = get_words()
+
+    secret_word = get_word()
     chances = 10
     guessed_letters = []
-    word_to_guess = []
+    blank_word = []
+    letters_to_guess = []
 
+    play = True
     while play:
         print('\nGuess the following word! Please select a letter between A-Z.\n')
 
         # create blank version of the secret word
         for letter in secret_word:
-            word_to_guess.append('_')
+            blank_word.append('_')
+            letters_to_guess.append(letter)
         
         # print out the blank version of the secret word
-        for i in word_to_guess:
+        for i in blank_word:
             print(i, end=" ")
         
         # print an empty line so that the code looks better
         print("\n")
 
+        # core game logic
+        while chances != 0:
+            guess = input("Guess a letter ==> ")
+            if guess in letters_to_guess:
+                print(f"{guess} is in the letters_to_guess")
+            else:
+                print("letter not found in word.")
+
         play = False
 
 # a function to store a list of random words and returns a random word from the list
-def get_words():
+def get_word():
     random_words = ["apple", "orange", "lime"]
     chosen_word = random.choice(random_words)
     return chosen_word
@@ -35,13 +47,6 @@ def get_words():
 def instructions():
     print("\nH A N G  M A N")
     print("\nWelcome to Hangman! A word will be chosen at random and \nyou must try to guess the word correctly letter by letter \nbefore you run out of attempts. Good luck!\n")
-
-# def play():
-#     user_choice = input("Are you ready to play? (y)es or (n)o\n")
-#     if user_choice == "n":
-#         return False
-#     elif user_choice == "y":
-#         return True
 
 if __name__ == "__main__":
     main()
